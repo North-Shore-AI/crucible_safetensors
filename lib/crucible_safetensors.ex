f3a604b2af1df66781c484716dd03e7eddd46f2e
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-05-22 12:40:37 -1000

    Initial Mix scaffold

---
+++ lib/crucible_safetensors.ex
@@ -0,0 +1,18 @@
+defmodule CrucibleSafetensors do
+  @moduledoc """
+  Documentation for `CrucibleSafetensors`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CrucibleSafetensors.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crucible_safetensors_test.exs
@@ -0,0 +1,8 @@
+defmodule CrucibleSafetensorsTest do
+  use ExUnit.Case
+  doctest CrucibleSafetensors
+
+  test "greets the world" do
+    assert CrucibleSafetensors.hello() == :world
+  end
+end
